719d5faf477014cec1f153af6349d5b4d25b83a3
nacharapol <nacharapol.b@gmail.com>
2020-07-31 09:22:24 +0700

    Revert "Update index.php"
    This reverts commit e896d4c63285f19f91540ff9cb04187a0356b9d3.

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
         <aside class="menu-sidebar2">
             <div class="logo">
                 <a href="#">
-                    <img src="images/icon/logo-white.png" alt="Biogas Water Pumping System" />
+                    <img src="images/icon/logo-white.png" alt="Administrator" />
                 </a>
             </div>
             <div class="menu-sidebar2__content js-scrollbar1">
@@ -100,7 +100,7 @@
                         <div class="header-wrap2">
                             <div class="logo d-block d-lg-none">
                                 <a href="#">
-                                    <img src="images/icon/logo-white.png" alt="Biogas Water Pumping System" />
+                                    <img src="images/icon/logo-white.png" alt="CoolAdmin" />
                                 </a>
                             </div>
                             <div class="header-button2">
@@ -125,13 +125,13 @@
             <aside class="menu-sidebar2 js-right-sidebar d-block d-lg-none">
                 <div class="logo">
                     <a href="#">
-                        <img src="images/icon/logo-white.png" alt="Administraor" />
+                        <img src="images/icon/logo-white.png" alt="Cool Admin" />
                     </a>
                 </div>
                 <div class="menu-sidebar2__content js-scrollbar2">
                     <div class="account2">
                         <div class="image img-cir img-120">
-                            <img src="images/icon/avatar-big-01.jpg" alt="Administraor" />
+                            <img src="images/icon/avatar-big-01.jpg" alt="John Doe" />
                         </div>
                         <h4 class="name">Administraor</h4>
                         <a href="#">หน้าหลัก</a>
